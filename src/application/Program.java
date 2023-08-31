729aa43507c42bc84e17893863bed1343941d1ed
João Victor Carvalho <carvalhojoaovi20@gmail.com>
2023-08-30 21:58:51 -0300

    feat: add components and try catch

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,42 @@
 package application;
-
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
+import entities.Product;
+
 public class Program {
-	public static void main(String[]args) {
+	public static void main(String[]args) throws ParseException {
 		
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+		
+		List<Product> list = new ArrayList<>();
+		
+		System.out.println("Type file path: ");
+		String sourceFileStr = sc.nextLine();
+		
+		File sourceFile = new File(sourceFileStr);
+		String sourceFolderStr = sourceFile.getParent();
+		
+		System.out.println(sourceFolderStr);
+		
+		boolean sucess = new File(sourceFolderStr + "\\out").mkdir();
+		System.out.println("Folder created: " + sucess);
+		
+		String targetFileStr = sourceFolderStr + "\\out\\summary.csv";
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(sourceFileStr))){
+			
+	}catch (IOException e) {
+		System.out.println("Error writing file " + e.getMessage());
+	}
 		
 		
 		
